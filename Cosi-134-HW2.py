a9206386eb2fb2c8eda0507d041b147d4287aa97
powcoder <powcoder@163.com>
2022-11-26 14:14:33 +0800

    COSI 134A StatNLP Viterbi LSTM CRF

---
+++ Cosi-134-HW2.py
@@ -0,0 +1,16 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+
+# # HW2
+# 
+# COSI-134A: StatNLP
+# 
+# 
+# Implement the Viterbi algorithm, the forward algorithm, as well as the scoring function for the LSTM-CRF model.
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# COSI 134A StatNLP Viterbi LSTM CRF 
+# 加微信 powcoder
+
+# Programming Help Add Wechat powcoder
+
+# Email: powcoder@163.com
+
